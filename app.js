d0125848b25bc7a19197214fdcc82c80980332be
Tanveer Singh <tanveer.singh926@gmail.com>
2019-04-30 23:52:41 +0800

    setup completed for mongo db and interactd with db

--- app.js
+++ app.js
@@ -1,12 +1,72 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const graphQlHttp = require('express-graphql')
+const { buildSchema } = require('graphql')
+const mongoose = require('mongoose')
+
+const Event = require('./models/event')
 
 const app = express()
 
 app.use(bodyParser.json())
 
-app.get('/', (req, res, next) => {
-  res.send(200)
-})
+app.use('/graphql', graphQlHttp({
+  schema: buildSchema(`
+    type Event {
+        _id: ID!
+        title: String!
+        description: String!
+        price: Float!
+        date: String
+    }
 
-app.listen(3000)
+    input EventInput {
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+    }
+
+    type RootQuery {
+        events: [Event!]!
+    }
+
+    type RootMutation {
+        createEvent(eventInput: EventInput): Event
+    }
+
+    schema {
+        query:  RootQuery
+        mutation: RootMutation
+    }
+  `),
+  rootValue: {
+    events: () => {
+      return events
+    },
+    createEvent: (args) => {
+      const event = new Event({
+        title: args.eventInput.title,
+        description: args.eventInput.description,
+        price: +args.eventInput.price,
+        date: new Date(args.eventInput.date)
+      })
+
+      return event.save().then(result => {
+        console.log(result)
+        return {...result._doc}
+      }).catch(err => {
+        console.log(err)
+        throw err
+      })
+      
+    }
+  },
+  graphiql: true
+}));
+
+mongoose.connect(`
+  mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-qsjb0.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`
+  ).then(() => {
+    app.listen(3000)
+  }).catch(err => console.log(err))
